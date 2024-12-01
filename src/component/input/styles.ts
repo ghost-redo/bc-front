e3198de450af76e78c99d68b76e8d5f9ac3772b4
ghost-redo <devredo98@gmail.com>
2024-12-01 14:15:31 +0900

    feat: 입력창 컴포넌트 스타일링 완료

--- src/component/input/styles.ts
+++ src/component/input/styles.ts
@@ -1,5 +1,23 @@
 import { cva } from "@/styled-system/css";
 
 export const inputStyles = cva({
-  base: {},
+  base: {
+    height: "32px",
+    background: "white",
+    fontSize: "14px",
+    fontWeight: 400,
+    paddingInline: "12px",
+    _placeholder: {
+      color: "#a1a1aa",
+    },
+    borderRadius: "8px",
+    border: "1px solid #e4e4e7",
+    _hover: {
+      border: "1px solid #4A7CFE",
+    },
+    _focus: {
+      border: "1px solid #4A7CFE",
+      boxShadow: "0px 0px 0px 2px rgba(74, 124, 254, 0.15)",
+    },
+  },
 });
